8b793493c381bbf8be0a3d589741ee70a0c1e2e7
DevLSeH <dev.sehyeon@gmail.com>
2023-05-10 13:00:43 +0900

    can use Korean for nameSpace/ add scroll in chat-box

--- app.js
+++ app.js
@@ -3,15 +3,29 @@ const server = app.listen(3000, () => {});
 const SocketIO = require("socket.io");
 const io = SocketIO(server, {path:"/socket.io"});
 const mysql = require("mysql2");
+const dotenv = require("dotenv");
+dotenv.config();
 const connection = mysql.createConnection({
   host:'localhost',
   user:'root',
   port:'3306',
-  password:'shaan1028!',
+  password:process.env.mySQl_PW,
   database:'socketio',
 })
 
 connection.connect();
+connection.query(
+  "delete from rooms",(err, result)=>{
+    if(!err){console.log("DB<rooms> was reset");}
+    else{throw(err);};
+  }
+);
+connection.query(
+  "delete from webchat",(err,result)=>{
+    if(!err){console.log("DB<webchat> was reset");}
+    else{throw(err);};
+  }
+)
 
 function messages(roomName){
   this.roomName = roomName;
@@ -47,19 +61,22 @@ function createRoom(roomName){
 
       connection.query(
         "INSERT INTO webchat (userId, msg, time, room) VALUES (?,?,?,?)",
-        [msg['userId'],msg['msg'],msg['time'],msg['room']], () =>{
-          console.log("data inserted in <webchat>");
-          connection.query(
-            "SELECT * FROM webchat WHERE room = ?",[msg['room']],(err, result)=>{
-              console.log("data found");
-              socket.emit("msg", result);
-              socket.broadcast.emit("msg",result);
-              console.log(result);
-            }
-          )
+        [msg['userId'],msg['msg'],msg['time'],msg['room']], (err) =>{
+          if(!err){
+            console.log("data inserted in <webchat>");
+            connection.query(
+              "SELECT * FROM webchat WHERE room = ? order by cast(id as char)",[msg['room']],(err, result)=>{
+                console.log("data found");
+                socket.emit("msg", result);
+                socket.broadcast.emit("msg",result);
+                console.log(result);
+              }
+            )
+          }else{throw(err);};
         }
       );
 
+      ////use global variable for save msgLog code
       //msgLog.push(msg);
       //socket.emit("msg",msgLog);
       //socket.broadcast.emit("msg", msgLog);
@@ -92,19 +109,35 @@ app.get("/", (req,res)=>{
 })
 lobby.on("connection", (socket)=>{
   console.log("lobby connected");
+  connection.query(
+    "select room from rooms group by room",(err, result)=>{
+      nameSpace = result;
+      nameSpace = nameSpace.map(rooms => rooms.room);
+      console.log(nameSpace);
+      return;
+    }
+  );
   
-  socket.on("create", (roomInfo)=>{
-  const roomName = roomInfo['roomName'];
+  socket.on("create",(roomInfo)=>{
+  const roomName = encodeURI(roomInfo['roomName']);
   if(nameSpace.includes(roomName)){
-    console.log("join room"+roomName);
+    console.log("join room :"+roomName);
     socket.emit("created", roomName);
     return;
   }
-  nameSpace.push(roomName);
+    
+  //nameSpace.push(roomName);
+  connection.query(
+    "insert into rooms (room) value (?)",[roomName],()=>{
+      console.log("saved roomName");
+    }
+  );
   console.log("create room :"+roomName);
   createRoom(roomName);
   socket.emit("created", roomName);
-})
+  return;
+  }
+  );
 })
     
 createRoom("default");
